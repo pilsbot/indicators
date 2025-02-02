612796d4a9c2dcff78126a4f6ad86d0fcc55de12
Pascal Pieper <accounts@pascalpieper.de>
2025-02-02 20:57:53 +0000

    Not optimal, but less flickery solution

--- src/stvo_translator.cpp
+++ src/stvo_translator.cpp
@@ -6,12 +6,15 @@
 
 #include <chrono>
 #include <functional>
+#include <optional>
 #include <memory>
 #include <string>
 #include <vector>
 
 class AckermannToLightingNode : public rclcpp::Node
 {
+    using TimePoint = std::chrono::time_point<std::chrono::system_clock>;
+
     struct Parameter
     {
         std::string ackermann_topic_from;
@@ -20,6 +23,8 @@ class AckermannToLightingNode : public rclcpp::Node
         double turning_threshold_rad;
         bool hazard_when_backing_up;
         double backwards_hazard_m_s;
+        double min_velocity_diff_for_brake_m_s;
+        std::chrono::duration<double> min_brake_time;
 
         bool tagfahrlicht;
 
@@ -30,22 +35,24 @@ class AckermannToLightingNode : public rclcpp::Node
 
     } param_;
 
-    using Time = std::chrono::time_point<std::chrono::system_clock>;
-
-  public:
+public:
     AckermannToLightingNode()
-    : Node("pilsbot_stvo_converter"), last_state_({0}), last_publish_(Time::min())
+    : Node("pilsbot_stvo_converter"), last_state_({0})
     {
         this->declare_parameter<std::string>("ackermann_topic_from", "pilsbot_velocity_controller/cmd_vel");
+        // TODO: Get negative acceleration from Sackermann
+
         this->declare_parameter<std::string>("prefix_lighting", "");
 
         this->declare_parameter<double>("turning_threshold_rad", .31415);
         this->declare_parameter<bool>("hazard_when_backing_up", true);
         this->declare_parameter<double>("backwards_hazard_m_s", .5);
+        this->declare_parameter<double>("min_brake_time_s", 1.);
+        this->declare_parameter<double>("min_velocity_diff_for_brake_m_s", .25);
 
         this->declare_parameter<bool>("tagfahrlicht", true);
 
-        this->declare_parameter<uint8_t>("brake_intensity", 0xF0);
+        this->declare_parameter<uint8_t>("brake_intensity", 0xE0);
         this->declare_parameter<uint8_t>("indicator_intensity", 0xB0);
         this->declare_parameter<uint8_t>("tagfahrlicht_intensity", 0x80);
         this->declare_parameter<uint8_t>("headlights_intensity", 0xA0);
@@ -56,6 +63,8 @@ class AckermannToLightingNode : public rclcpp::Node
         param_.turning_threshold_rad = this->get_parameter("turning_threshold_rad").as_double();
         param_.hazard_when_backing_up = this->get_parameter("hazard_when_backing_up").as_bool();
         param_.backwards_hazard_m_s = this->get_parameter("backwards_hazard_m_s").as_double();
+        param_.min_velocity_diff_for_brake_m_s = this->get_parameter("min_velocity_diff_for_brake_m_s").as_double();
+        param_.min_brake_time = std::chrono::duration<double>{this->get_parameter("min_brake_time_s").as_double()};
         param_.tagfahrlicht = this->get_parameter("tagfahrlicht").as_bool();
 
         param_.brake_intensity = this->get_parameter("brake_intensity").as_int();
@@ -63,7 +72,6 @@ class AckermannToLightingNode : public rclcpp::Node
         param_.tagfahrlicht_intensity = this->get_parameter("tagfahrlicht_intensity").as_int();
         param_.headlights_intensity = this->get_parameter("headlights_intensity").as_int();
 
-        lightingPublishers_.reserve(indicators::topics.size());
         for (const auto& topic : indicators::topics)
         {
             const auto& offset = getOffsetFromTopic(topic);
@@ -75,6 +83,9 @@ class AckermannToLightingNode : public rclcpp::Node
                     param_.prefix_lighting + indicators::getTopicName(topic), rclcpp::SensorDataQoS());
         }
 
+        RCLCPP_INFO(this->get_logger(),
+                "Brake-time: " + std::to_string(std::chrono::duration_cast<std::chrono::milliseconds>(param_.min_brake_time).count()) + "ms");
+
         // Initial publish to default values
         topic_callback(std::make_shared<ackermann_msgs::msg::AckermannDriveStamped>(last_msg_));
 
@@ -85,20 +96,32 @@ class AckermannToLightingNode : public rclcpp::Node
                       &AckermannToLightingNode::topic_callback, this, std::placeholders::_1));
     }
 
-  private:
+private:
     void topic_callback(const ackermann_msgs::msg::AckermannDriveStamped::SharedPtr msg)
     {
-        // TODO
         // RCLCPP_INFO(this->get_logger(), "%f %f", msg->drive.speed, msg->drive.steering_angle);
 
-        std::array<uint8_t, indicators::topics.size()> new_state = last_state_;
+        auto new_state = last_state_;
+
+        const bool brake_on = (param_.min_velocity_diff_for_brake_m_s + std::abs(msg->drive.speed)) < std::abs(last_msg_.drive.speed)
+                                || msg->drive.speed == 0;
+
+        if (brake_on)
+        {
+            start_brake_ = std::chrono::system_clock::now();
+        }
+
+        const bool brake_still_on = (start_brake_ && (std::chrono::system_clock::now() < *start_brake_ + param_.min_brake_time));
+
+        if (!brake_still_on)
+        {
+            start_brake_ = std::nullopt;
+        }
 
         // brakes
-        if (std::abs(msg->drive.speed) < std::abs(last_msg_.drive.speed) || msg->drive.speed == 0)
+        if (brake_on || brake_still_on)
         {
-            // TODO: Instead of last_msg_, compare against actual odometry
-            // RCLCPP_INFO(this->get_logger(), "brake light on");
-            // TODO: Intensity in config
+            // RCLCPP_INFO(this->get_logger(), "brake light on because nowbrake: " + std::to_string(brake_on) + " and brake_Still_on: " + std::to_string(brake_still_on) );
             new_state[getOffsetFromTopic(indicators::Topic::brake)] = param_.brake_intensity;
         }
         else
@@ -106,7 +129,6 @@ class AckermannToLightingNode : public rclcpp::Node
             // RCLCPP_INFO(this->get_logger(), "brake light off");
             new_state[getOffsetFromTopic(indicators::Topic::brake)] = 0x00;
         }
-
 
         // turning signals
         const bool warnblink_on_backwards = true;   // TODO: Make Parameter
@@ -164,14 +186,16 @@ class AckermannToLightingNode : public rclcpp::Node
         return static_cast<std::underlying_type_t<indicators::Topic>>(topic);
     }
 
-    std::vector<rclcpp::Publisher<std_msgs::msg::Byte>::SharedPtr> lightingPublishers_;
+    std::map<size_t, rclcpp::Publisher<std_msgs::msg::Byte>::SharedPtr> lightingPublishers_;
     rclcpp::Subscription<ackermann_msgs::msg::AckermannDriveStamped>::SharedPtr steeringInputSubscription_;
 
     std::array<uint8_t, indicators::topics.size()> last_state_;
     ackermann_msgs::msg::AckermannDriveStamped last_msg_;
 
-    // todo: Use with timeout to enable hazard light if too long no commands
-    Time last_publish_;
+    // todo: hazard light if too long no commands?
+
+    std::optional<TimePoint> start_brake_;
+
 };
 
 int main(int argc, char * argv[])
